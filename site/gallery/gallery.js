7715337716ffc1cc24090e059ff37414fe84fcba
Elijah Gamble <elismobileautorepair@gmail.com>
2026-09-12 16:47:11 -1000

    Add first community gallery set

--- site/gallery/gallery.js
+++ site/gallery/gallery.js
@@ -4,4 +4,7 @@
   To add a picture: place it in public/gallery/ and add one line below.
 */
 window.NEARKAT_GALLERY = [
+  { src: 'gallery/community-001.png', title: 'Five NEARKAT mob members in suits' },
+  { src: 'gallery/community-002.png', title: 'Two NEARKAT mobsters beside a van' },
+  { src: 'gallery/community-003.png', title: 'NEARKAT fishing among Saturn\'s rings' }
 ];
