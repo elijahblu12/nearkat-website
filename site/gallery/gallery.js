5c59e51676f256bcb2d6ed0951cae519714e9a5e
Elijah Gamble <elismobileautorepair@gmail.com>
2026-09-12 15:46:37 -1000

    Clarify gallery is view only

--- site/gallery/gallery.js
+++ site/gallery/gallery.js
@@ -1,6 +1,6 @@
 /*
   OWNER-ONLY COMMUNITY GALLERY
-  Visitors can view and download these images, but there is no public upload form.
+  Visitors can view these images, but there is no public upload form.
   To add a picture: place it in public/gallery/ and add one line below.
 */
 window.NEARKAT_GALLERY = [
